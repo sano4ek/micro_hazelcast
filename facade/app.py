56b9bb7cb9c19cdcfea8d2c10a803e2b89c72df4
sano4ek <38867937+sano4ek@users.noreply.github.com>
2021-05-21 22:55:06 +0300

    task6

--- facade/app.py
+++ facade/app.py
@@ -1,20 +1,26 @@
 import random
 import uuid
+import pika
 import requests
 from flask import Flask, request
 
 app = Flask(__name__)
 
-messenger_service_url = "http://0.0.0.0:8089/messenger"
+messenger_service_url = (
+    "http://0.0.0.0:1122", "http://0.0.0.0:1123"
+)
 
 
 def get_rand_logging_client():
-    return random.choice(["http://0.0.0.0:8081/logging-service", "http://0.0.0.0:8082/logging-service", "http://0.0.0.0:8083/logging-service"])
+    return random.choice(["http://0.0.0.0:8011/logging-service", "http://0.0.0.0:8012/logging-service", "http://0.0.0.0:8013/logging-service"])
 
+def get_rand_messages_service_url() -> str:
+    return random.choice(messenger_service_url)
 
 @app.route('/facade-service', methods=['GET', 'POST'])
 def facade_service():
     if request.method == 'POST':
+        post_msg_to_mq(request.json.get('message'))
         logging_service_response = requests.post(
             url=get_rand_logging_client(),
             json={
@@ -26,9 +32,22 @@ def facade_service():
         return app.response_class(status=status)
     else:
         logging_service_response = requests.get(get_rand_logging_client())
-        messages_service_r = requests.get(messenger_service_url)
+        messages_service_r = requests.get(get_rand_messages_service_url())
         return str(logging_service_response.text) + ' : ' + str(messages_service_r.text)
+
+def post_msg_to_mq(msg: str):
+    mq_connection = pika.BlockingConnection(
+        pika.ConnectionParameters('127.0.0.1')
+    )
+    channel = mq_connection.channel()
+    channel.queue_declare(queue='mq_for_messages_service')
+    channel.basic_publish(
+        exchange='', routing_key="mq_for_messages_service",
+        body=msg,
+    )
+    print(f"[x] Sent: {msg}")
+    mq_connection.close()
 
 
 if __name__ == '__main__':
-      app.run(host='0.0.0.0', port=8080, debug=True)
+      app.run(host='0.0.0.0', port=1345, debug=True)
